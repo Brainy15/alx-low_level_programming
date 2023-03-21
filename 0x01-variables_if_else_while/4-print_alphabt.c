5122cd7527538c24fd5990b0f5fc8e9d1a98978d
Ismail Agboke <hargboke15@gmail.com>
2023-03-20 19:48:38 -0700

    progress

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints all alpahbets but not q and e
+ *
+ * Return: the return is 0
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
