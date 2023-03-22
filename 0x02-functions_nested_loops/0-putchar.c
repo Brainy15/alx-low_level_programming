9ab8e88976974bf4268986b0f684fb81b10fc475
Ismail Agboke <hargboke15@gmail.com>
2023-03-21 17:52:08 -0700

    jd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,8 @@
  * Description: 'prints _putchar to stdout'
  * Return: always 0
  */
-int main(void) 
-{	
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return 0;
+	return (0);
 }
